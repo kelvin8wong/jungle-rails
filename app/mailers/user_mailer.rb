75263c2330b6eeb59e1c1cb5d4d41164431faa81
kelvin8wong <kelvin8wong@gmail.com>
2017-11-10 20:20:20 +0000

    feature/order-receipt: generated mailer controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+  
+  def order_confirmation(order)
+    @order = order
+    mail(to: @order.email, subject: 'Here is your receipt!')
+  end
 end
