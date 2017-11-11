d4e6701deaca66646970f2e433d160520bad3b2a
kelvin8wong <kelvin8wong@gmail.com>
2017-11-11 01:03:46 +0000

    feature/rating: updated rating validation on review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
-  validates :product_id, :user_id, :rating  presence: true
+  validates :product_id, :user_id, :rating,  presence: true
+  validates :rating, :inclusion => { :in => 1..5 }
 
 end
