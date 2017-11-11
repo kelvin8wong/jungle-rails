3e1acc8c2d5cb88cd3a71fe71f7e21710f4a0615
kelvin8wong <kelvin8wong@gmail.com>
2017-11-11 06:19:17 +0000

    feature/rating: linked user with review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+  belongs_to :user
+  
   validates :product_id, :user_id, :rating,  presence: true
   validates :rating, :inclusion => { :in => 1..5 }
 
