596d48fc40368b849e7aca7da9b20ccbc55132b2
kelvin8wong <kelvin8wong@gmail.com>
2017-11-12 03:24:11 +0000

    feature/rating: defined create review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,20 @@
 class ReviewsController < ApplicationController
-  
+  before_filter :authorize
+
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    review = @product.reviews.new(reviews_params)
+    review.user = current_user
+    if review.save
+      redirect_to @product
+    end
+  end
+
+  private
+
+  def reviews_params
+    params.require(:review).permit(
+      :description,
+      :rating)
   end
 end
