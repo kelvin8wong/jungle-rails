79cc532aa817ebe87d1d3cd35f21901509a38811
kelvin8wong <kelvin8wong@gmail.com>
2017-11-12 19:57:40 +0000

    fixed stylings and added flash error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = "You have successfully signed up!"
       redirect_to '/'
     else
+      flash[:danger] = 'Invalid email/password combination'
       redirect_to '/signup'
     end
   end
