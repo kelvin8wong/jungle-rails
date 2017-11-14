842edcd724c9cce3806a8329d096ef4de8687d56
kelvin8wong <kelvin8wong@gmail.com>
2017-11-14 23:19:39 +0000

    validated user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
   has_many :reviews 
   
-  validates :first_name, :last_name,  presence: true,length: {maximum: 50}
+  validates :first_name, :last_name,  presence: true 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates_length_of :password, minimum: 8
+  validates :password, :confirmation => true
   has_secure_password
+
+ def self.authenticate_with_credentials (email, password)
+    user = User.find_by_email(email.downcase.delete " ")
+    if user && user.authenticate(password)
+      user
+    end
+ end
+
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before :each do
+    @user = User.create(first_name: 'rspectest', last_name: 'rspectest', email: 'rspectest@test.com', password: 'testtest', password_confirmation: 'testtest')
+  end
+
   describe "Validations:" do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:email) }
+    it { should validate_confirmation_of(:password) }
     it { is_expected.to have_secure_password }
+    it { should validate_length_of(:password).is_at_least(8) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value('test@test.com').for(:email) }
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    it "authenticates existing user" do
+      user = User.authenticate_with_credentials('rspectest@test.com', 'testtest')
+      expect(user).to eql(@user)
+    end
+  
+    it "does not authenticate user with wrong password" do
+      user = User.authenticate_with_credentials('rspectest@test.com', 'barbarbar')
+      expect(user).to be_nil
+    end
+
+    it 'accepts email with spaces' do
+      user = User.authenticate_with_credentials('    rspectest@test.com', 'testtest')
+      expect(user).to eql(@user)
+    end
+
+    it "doesn't consider case in the email" do
+      user = User.authenticate_with_credentials('rspectest@TEST.com', 'testtest')
+      expect(user).to eql(@user)
+    end
   end
 
 end
