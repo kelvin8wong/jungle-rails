aa95cda0d888e8fe78bf590068e1349bc32975a2
kelvin8wong <kelvin8wong@gmail.com>
2017-11-14 23:27:02 +0000

    changed notice message

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
-      redirect_to order, notice: 'Your Order has been placed.'
+      redirect_to order
       UserMailer.order_confirmation(order).deliver
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
